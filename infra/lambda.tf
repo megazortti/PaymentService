cd4fc30ef3b25ddd8a94036fe36d2b626dd51bd4
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-15 19:57:14 -0300

    fixing terraform configuration

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -18,8 +18,8 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda"
-  output_path = "${path.module}/lambda.zip"
+  source_dir  = "${path.module}/../lambda"
+  output_path = "${path.module}/../lambda.zip"
 }
 
 resource "aws_lambda_function" "hello" {
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,4 +1,5 @@
 aws_region          = "us-east-1"
-cloudflare_api_token = "seu_token_cloudflare_aqui"
-domain_name         = "seusite.app"
-cloudflare_zone_id  = "sua_zone_id_cloudflare"
+cloudflare_api_token = "${{ secrets.cloudflare_api_token }}"
+domain_name         = "mazzotti.app"
+cloudflare_zone_id  = "${{ secrets.cloudflare_zone_id }}"
+    

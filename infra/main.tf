1e4e05069434c7172a1b3d554ba60d424ff45837
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-16 20:09:29 -0300

    fix terraform

--- infra/main.tf
+++ infra/main.tf
@@ -11,8 +11,5 @@ module "payment_webhook_lambda" {
   function_name   = "payment_webhook_lambda"
   handler         = "handler.webhook"
   runtime         = "nodejs18.x"
-  environment     = {
-    MERCADOPAGO_API_KEY = var.mercadopago_api_key
-  }
   source_path     = "../lambdas/payment_webhook"
 }

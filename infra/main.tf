1d14e9aa77d7430c34b327a340e3be90672dcca7
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-16 00:00:03 -0300

    adding modularity and stuff

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,7 @@
 module "hello_lambda" {
-  source        = "../modules/lambda"
+  source        = "./modules/lambda"  # Subindo dois níveis
   function_name = "hello_lambda_minimal"
-  source_path   = "../../lambda_functions/hello"
+  source_path   = "./../lambda/create_payment"  # Caminho absoluto ou relativo corrigido
   handler       = "index.handler"
   runtime       = "nodejs18.x"
 }

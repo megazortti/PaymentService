25d382dbc9bfbc8728e1406a661811642a106822
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-18 00:31:27 -0300

    remake stuffs

--- infra/modules/apigateway/main.tf
+++ infra/modules/apigateway/main.tf
@@ -43,7 +43,3 @@ resource "aws_apigatewayv2_domain_name" "custom" {
     security_policy = "TLS_1_2"
   }
 }
-
-output "api_gateway_domain" {
-  value = aws_apigatewayv2_domain_name.custom.domain_name_configuration[0].target_domain_name
-}

9d6967d51b00330f1975039b9d896dc0df6c039f
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-18 01:06:39 -0300

    tst

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -27,6 +27,5 @@ resource "aws_lambda_permission" "allow_apigw" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 

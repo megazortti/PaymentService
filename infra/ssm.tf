ac48a011a3d5a675b1ac87e268ce32c1dcc2906d
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-18 17:42:24 -0300

    tst

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -1,19 +1,19 @@
 resource "aws_ssm_parameter" "mercadopago_access_token" {
-  name        = "/secrets/mercadopago/pix_token"
+  name        = "/secrets/mercadopago/access_token"
   type        = "SecureString"
   value       = "CHANGE_HERE"
   description = "Token de acesso do MercadoPago para pagamentos"
 }
 
 resource "aws_ssm_parameter" "mercadopago_client_secret_token" {
-  name        = "/secrets/mercadopago/pix_token"
+  name        = "/secrets/mercadopago/client_secret"
   type        = "SecureString"
   value       = "CHANGE_HERE"
   description = "Token de acesso do MercadoPago secret"
 }
 
 resource "aws_ssm_parameter" "mercadopago_notification_token" {
-  name        = "/secrets/mercadopago/pix_token"
+  name        = "/secrets/mercadopago/notification_token"
   type        = "SecureString"
   value       = "CHANGE_HERE"
   description = "Token de acesso do MercadoPago para notificações"

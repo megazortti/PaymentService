2fa109116670133f1e68cb15c1b703f5fe485650
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-17 23:57:47 -0300

    test

--- infra/modules/lambda/variables.tf
+++ infra/modules/lambda/variables.tf
@@ -1,6 +1,25 @@
-variable "function_name" { type = string }
-variable "source_path"   { type = string }
-variable "handler"       { type = string }
-variable "runtime"       { type = string }
-variable "memory_size"   { type = number, default = 128 }
-variable "timeout"       { type = number, default = 3 }
+variable "function_name" {
+  type = string
+}
+
+variable "source_path" {
+  type = string
+}
+
+variable "handler" {
+  type = string
+}
+
+variable "runtime" {
+  type = string
+}
+
+variable "memory_size" {
+  type    = number
+  default = 128
+}
+
+variable "timeout" {
+  type    = number
+  default = 3
+}

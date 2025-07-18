4b5b08bdbf4316cf1af28cfcd5c2ad44f1d2f539
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-18 17:15:25 -0300

    tst

--- infra/ssm.tf
+++ infra/ssm.tf
@@ -1,7 +1,6 @@
 resource "aws_ssm_parameter" "mercadopago_token" {
   name        = "/secrets/mercadopago/pix_token"
   type        = "SecureString"
-  value       = var.mercadopago_token
+  value       = "CHANGE_HERE"
   description = "Token de acesso do MercadoPago para pagamentos via Pix"
-  overwrite   = true
 }

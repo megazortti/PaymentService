0f9190da1f10555118d9481d85967f71251ace3b
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-18 14:24:04 -0300

    tst

--- infra/main.tf
+++ infra/main.tf
@@ -13,9 +13,31 @@ module "apigw" {
   cloudflare_zone_id = var.cloudflare_zone_id
 }
 
+module "payment_webhook" {
+  source      = "./modules/lambda"
+  lambda_name = "payment-webhook"
+  handler     = "index.handler"
+  runtime     = "nodejs18.x"
+  source_path = "${path.module}/../lambda/payment_webhook"
+}
+
+module "apigw_payment_webhook" {
+  source           = "./modules/apigateway"
+  lambda_arn       = module.payment_webhook.lambda_arn
+  custom_domain    = "pay.mazzotti.app"
+  cloudflare_zone_id = var.cloudflare_zone_id
+}
+
 module "cloudflare" {
   source        = "./modules/cloudflare"
   domain_name   = var.domain_name
   zone_id       = var.cloudflare_zone_id
   target_domain = module.apigw.api_gateway_domain
+}
+
+module "cloudflare" {
+  source        = "./modules/cloudflare"
+  domain_name   = "pay.mazzotti.app"
+  zone_id       = var.cloudflare_zone_id
+  target_domain = module.apigw_payment_webhook.api_gateway_domain
 }

8b31db8e6719b545a93f9f8b2f90a4f17f3700ff
Victor Mazzotti <mazzotti.vlm@gmail.com>
2025-07-18 15:45:49 -0300

    tst

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -1,3 +1,11 @@
+output "lambda_exec_role_name" {
+  value = aws_iam_role.lambda_exec.name
+}
+
+output "lambda_exec_role_arn" {
+  value = aws_iam_role.lambda_exec.arn
+}
+
 output "lambda_arn" {
   value = aws_lambda_function.lambda.arn
 }
